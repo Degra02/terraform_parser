d2c5e1b09e7b17b129143717790cedafc253c523
Degra02 <filippodegrandi02@gmail.com>
2024-07-29 11:01:44 +0200

    Fix error

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub mod state_reresentation;
+pub mod state_representation;
 pub mod values_representation;
 #[cfg(test)]
 pub mod tests;
@@ -7,7 +7,7 @@ pub mod tests;
 pub struct TerraformParser {}
 
 impl TerraformParser {
-    pub fn parse_state(state: &str) -> Result<state_reresentation::StateRepresentation, serde_json::Error> {
+    pub fn parse_state(state: &str) -> Result<state_representation::StateRepresentation, serde_json::Error> {
         serde_json::from_str(state)
     }
 }
